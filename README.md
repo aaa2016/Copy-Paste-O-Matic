dc986a3e4e39d1897c5fe12d27425167a0ee1192
Artur A. <artur.arikainen@protonmail.com>
2026-04-02 01:17:19 +0100

    v0.2 - added more features
    Buttons now generate dynamically from config.js
    Updated README and added screenshot

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Copy-Paste-O-Matic
- Quick text copy paster
+
+## Table of Contents
+1. [Purpose](#purpose)
+2. [Features yet to be implemented](#features)
+3. [Screenshot](#screenshot)
+
+## Purpose<a name="purpose"></a>
+
+This tool allows a user to quickly paste snippets of text by clicking the corresponding button.
+
+The text is appended to the text area on the right.
+
+Click 'COPY TEXT' to copy the contents of the text area to your clipboard, for pasting elsewhere.
+
+The sections, buttons, and text to be inserted from each button are found in the config.js file - this file must be saved in the same directory as the .html file of this tool.
+
+Each snippet of text can be copied as many times as needed.
+
+Any text that is manually inserted or edited in the text area will also be copied to the clipboard.
+
+## Features yet to be implemented<a name="features"></a>
+
+* Tooltips that display the text to be copied when a button is hovered over
+* Supporting multiple workflows, each with their own sections and buttons (will have dropdown selector)
+
+## Screenshot<a name="screenshot"></a>
+
+![Screenshot of tool](https://github.com/aaa2016/Copy-Paste-O-Matic/blob/master/screenshot.png)
---
+++ config.js
@@ -0,0 +1,57 @@
+var brandColor = "#555555"; // Default is otherwise: #555555
+var brandTextColor = "white"; // Default is otherwise: white
+
+var configInfo = [
+
+	// Section 1
+	[
+		["Section 1"],
+		["Text Aa","- This is text Aa.\n"],
+		["Text Bb","- This is text Bb.\n"],
+		["Text Cc","- This is text Cc.\n"]
+	],
+	
+	// Section 2
+	[
+		["Section 2"],
+		["Text Dd","- This is text Dd.\n"],
+		["Text Ee","- This is text Ee.\n"],
+		["Text Ff","- This is text Ff.\n"]
+		],
+	
+	// Section 3
+	[
+		["Section 3"],
+		["Text Aa","- This is text Aa.\n"],
+		["Text Bb","- This is text Bb.\n"],
+		["Text Cc","- This is text Cc.\n"],
+		["Text Aaa","- This is text Aaa.\n"],
+		["Text Bbb","- This is text Bbb.\n"],
+		["Text Ccc","- This is text Ccc.\n"]
+	],
+	
+	// Section 4
+	[
+		["Section 4"],
+		["Text Dd","- This is text Dd.\n"],
+		["Text Ee","- This is text Ee.\n"],
+		["Text Ff","- This is text Ff.\n"],
+		["Text Ddd","- This is text Ddd.\n"],
+		["Text Eee","- This is text Eee.\n"],
+		["Text Fff","- This is text Fff.\n"]
+		],
+	
+	// Section 5
+	[
+		["Section 5"],
+		["Text Xx","- This is text Xx.\n"],
+		["Text Yy","- This is text Yy.\n"],
+		["Text Zz","- This is text Zz.\n"]
+	]
+	
+	// Note: Last button in each section doesn't have comma after square bracket
+	// Note: Last section doesn't have comma after square bracket
+
+]; // End of configInfo array
+
+console.log("Config file has been loaded.");
